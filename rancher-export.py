93a760cf15ddd736c6abcf421a258754defc3db9
Eugene Brodsky <ebr@users.noreply.github.com>
2016-10-12 14:39:25 -0400

    use pycharm, add pycharm settings

--- rancher-export.py
+++ rancher-export.py
@@ -1,45 +1,26 @@
-import os, sys, json, requests, gdapi
+import os, sys, json, requests, cattle
 
 rancher_api_base_url = os.environ['RANCHER_API_BASE_URL']
 rancher_api_access_key = os.environ['RANCHER_API_ACCESS_KEY']
 rancher_api_secret_key = os.environ['RANCHER_API_SECRET_KEY']
 
-# gather an environment
-
-# class Rancher(object):
-#   """The entire Rancher setup """
-#   def __init__(self, access_key, secret_key, url):
-#     super(Rancher, self).__init__()
-#     self.client = gdapi.Client(access_key, secret_key, url)
-  
-#   def environments(self):
-
-    
-
-# class Environment(object):
-#   """Rancher Environment"""
-#   def __init__(self, client, env_id):
-#     # super(Environment, self).__init__()
-    
-
-
-#     self.name = client.
-
-#     self.arg = arg
-    
-
-# def main():
-
-
 
 if __name__ == '__main__':
-  # main()
+    # main()
 
-  rancher = gdapi.Client(access_key=rancher_api_access_key, secret_key=rancher_api_secret_key, url=rancher_api_base_url)
+    rancher = cattle.Client(access_key=rancher_api_access_key,
+                            secret_key=rancher_api_secret_key,
+                            url=rancher_api_base_url)
 
-  environments = rancher.list('project').data
+    environments = rancher.list('project').data
+
+    stacks = rancher.list('environment').data
+
+    for env in environments:
+        pass
 
 
 
-  # for project in env_data:
-  #   print ('{:10} {:5} {:5}'.format(project.id, project.name, project.links.composeConfig))
+
+        # for project in env_data:
+        #   print ('{:10} {:5} {:5}'.format(project.id, project.name, project.links.composeConfig))
